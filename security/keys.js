5f1b65c28fd880da6036a1d2ceddb7ef2fb2e7b2
Divish1032 <30887146+Divish1032@users.noreply.github.com>
2020-04-30 00:55:36 +0530

    Update

--- security/keys.js
+++ security/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     admin : {
         email : 'ecell@itbhu.ac.in',
-        password : 'ECELL_IITBHU@20',
-        mongoDB : 'mongodb://esummit:qwerty123@ds243148.mlab.com:43148/esummit',
+        password : 'ECELL_IITBHU',
+        mongoDB : 'mongodb://esummit:@ds243148.mlab.com:43148/esummit',
         from_email : 'E-CELL IIT(BHU) Varanasi <ecell@itbhu.ac.in>'
     }
 }
